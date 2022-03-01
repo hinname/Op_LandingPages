9643a7e98f582d520eb3bdc304ea89638d12ab0c
All Resende <allanresendegomes@outlook.com>
2022-03-01 12:22:53 -0300

    pagina melhorada na versao mobile

--- allan/script.js
+++ allan/script.js
@@ -1,19 +1,23 @@
-let ColorName = document.getElementById('ColorName')
+let colorName = document.getElementById('colorName')
 let purpleOption = document.getElementById('purple')
 let whiteOption = document.getElementById('white')
 let blackOption = document.getElementById('black')
 
-const allModels = document.getElementById('allModels')
-const purpleImage = document.getElementById('purpleModel')
-const whiteImage = document.getElementById('whiteModel')
-const blackImage = document.getElementById('blackModel')
+const allModels = document.getElementsByClassName('allModels')
+const purpleImage = document.getElementsByClassName('purpleModel')
+const whiteImage = document.getElementsByClassName('whiteModel')
+const blackImage = document.getElementsByClassName('blackModel')
 
-const bigBallW = '1.25rem'
-const bigBallH = '1.25rem'
-const bigBallBorder = '2px'
-const normalBallW = '1.1rem'
-const normalBallH = '1.1rem'
-const normalBallBorder = '1.3px'
+const bigBallW = '1.45rem'
+const bigBallH = '1.45rem'
+const bigBallBorder = '2.5px'
+const normalBallW = '1.25rem'
+const normalBallH = '1.25rem'
+const normalBallBorder = '1.5px'
+
+let bigContainer = document.getElementById('bigContainer')
+let hamburgerMenu = document.querySelector('.hamburgerMenu')
+let overlay = document.querySelector('.overlay')
 
 function purpleModel() {
   purpleOption.style.width = bigBallW
@@ -28,16 +32,18 @@ function purpleModel() {
   blackOption.style.height = normalBallH
   blackOption.style.borderWidth = normalBallBorder
 
-  allModels.style.display = 'none'
-  allModels.style.opacity = '0'
-  purpleImage.style.display = 'flex'
-  purpleImage.style.opacity = '1'
-  whiteImage.style.display = 'none'
-  whiteImage.style.opacity = '0'
-  blackImage.style.display = 'none'
-  blackImage.style.opacity = '0'
-
   colorName.textContent = 'Purple-ish'
+
+  for (i = 0; i <= allModels.length; i++) {
+    allModels[i].style.display = 'none'
+    allModels[i].style.opacity = '0'
+    purpleImage[i].style.display = 'flex'
+    purpleImage[i].style.opacity = '1'
+    whiteImage[i].style.display = 'none'
+    whiteImage[i].style.opacity = '0'
+    blackImage[i].style.display = 'none'
+    blackImage[i].style.opacity = '0'
+  }
 }
 
 function whiteModel() {
@@ -53,16 +59,19 @@ function whiteModel() {
   blackOption.style.height = normalBallH
   blackOption.style.borderWidth = normalBallBorder
 
-  allModels.style.display = 'none'
-  allModels.style.opacity = '0'
-  purpleImage.style.display = 'none'
-  purpleImage.style.opacity = '0'
-  whiteImage.style.display = 'flex'
-  whiteImage.style.opacity = '1'
-  blackImage.style.display = 'none'
-  blackImage.style.opacity = '0'
-
   colorName.textContent = 'Clearly White'
+
+  for (i = 0; i <= allModels.length; i++) {
+    allModels[i].style.display = 'none'
+    allModels[i].style.opacity = '0'
+    purpleImage[i].style.display = 'none'
+    purpleImage[i].style.opacity = '0'
+    whiteImage[i].style.display = 'flex'
+    whiteImage[i].style.opacity = '1'
+    blackImage[i].style.display = 'none'
+    blackImage[i].style.opacity = '0'
+    console.log(allModels[i])
+  }
 }
 
 function blackModel() {
@@ -78,14 +87,29 @@ function blackModel() {
   blackOption.style.height = bigBallH
   blackOption.style.borderWidth = bigBallBorder
 
-  allModels.style.display = 'none'
-  allModels.style.opacity = '0'
-  purpleImage.style.display = 'none'
-  purpleImage.style.opacity = '0'
-  whiteImage.style.display = 'none'
-  whiteImage.style.opacity = '0'
-  blackImage.style.display = 'flex'
-  blackImage.style.opacity = '1'
-
   colorName.textContent = 'Just Black'
+
+  for (i = 0; i <= allModels.length; i++) {
+    allModels[i].style.display = 'none'
+    allModels[i].style.opacity = '0'
+    purpleImage[i].style.display = 'none'
+    purpleImage[i].style.opacity = '0'
+    whiteImage[i].style.display = 'none'
+    whiteImage[i].style.opacity = '0'
+    blackImage[i].style.display = 'flex'
+    blackImage[i].style.opacity = '1'
+  }
 }
+
+function menu() {
+  hamburgerMenu.addEventListener('click', () => {
+    bigContainer.classList.toggle('menuActive')
+    document.body.classList.toggle('stopScrolling')
+  })
+
+  overlay.addEventListener('click', () => {
+    bigContainer.classList.remove('menuActive')
+    document.body.classList.remove('stopScrolling')
+  })
+}
+menu()
